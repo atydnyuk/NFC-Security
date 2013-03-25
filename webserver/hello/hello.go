f739e5c8db636a6ff7ad4859276a107170e7a6a7
atydnyuk <atydnyuk@gmail.com>
2013-03-24 21:12:23 -0400

    made the default page the log page...studying for exam also

--- webserver/hello/hello.go
+++ webserver/hello/hello.go
@@ -3,12 +3,22 @@ package hello
 import (
 	"fmt"
 	"net/http"
+	"html/template"
 )
 
+var logTemplate = template.Must(template.New("log.html").ParseFiles("templates/log.html"))
+
 func init() {
-	http.HandleFunc("/",handler)
+	http.HandleFunc("/",root)
+}
+
+func root(w http.ResponseWriter, r *http.Request) {
+	if err := indexTemplate.Execute(w,nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w,"Hello World!")
+	fmt.Fprintf(w,"<html><head><script type=\"text/javascript\"src=\"/javascript/jquery.js\"></script></head>Hello World!<script>alert(1);</script></html>")
+	
 }
