64bcb9a9ec6831343d836b3650d683077bfec931
atydnyuk <atydnyuk@gmail.com>
2013-03-26 13:51:12 -0400

    Couldnt figure out how to get logs. used datastore. hacky..

--- webserver/hello/hello.go
+++ webserver/hello/hello.go
@@ -5,14 +5,24 @@ import (
 	"net/http"
 	"html/template"
 	"appengine"
-	"appengine/log"
+	"appengine/datastore"
+	"time"
 )
+
+type RequestRecord struct {
+	RemoteAddr string
+	Host string
+	Method string
+	Header string
+	URL string
+	Time time.Time
+}
 
 var logTemplate = template.Must(template.New("log.html").ParseFiles("templates/log.html"))
 
 func init() {
-	http.HandleFunc("/",root)
-	http.HandleFunc("/log",logprinter)
+	http.HandleFunc("/",logprinter)
+	http.HandleFunc("/log",root)
 }
 
 func root(w http.ResponseWriter, r *http.Request) {
@@ -22,22 +32,30 @@ func root(w http.ResponseWriter, r *http.Request) {
 }
 
 func logprinter(w http.ResponseWriter, r *http.Request) {
+	stringURL := fmt.Sprintf("%#v",r.URL)
+	stringHeader := fmt.Sprintf("%#v",r.Header)
 	c := appengine.NewContext(r)
-	query := &log.Query{
-    AppLogs:  true,
-    Versions: []string{"1"},
+    req := RequestRecord{
+    RemoteAddr:r.RemoteAddr,
+	Host:r.Host,
+	Method:r.Method,
+	Header:stringHeader,
+	URL:stringURL,
+	Time:time.Now(),
+	}
+	_, err := datastore.Put(c, datastore.NewIncompleteKey(c,"Record", nil), &req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
 	}
 	
-	for results := query.Run(c); ; {
-		record, err := results.Next()
-		if err == log.Done {
-			fmt.Fprintf(w,"Done processing results")
-			break
-		}
-		if err != nil {
-			c.Errorf("Failed to retrieve next log: %v", err)
-			break
-		}
-		fmt.Fprintf(w,"Saw record %v", record)
+    q := datastore.NewQuery("Record").Order("-Time").Limit(10)
+    records := make([]RequestRecord, 0, 10)
+    if _, err := q.GetAll(c, &records); err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+	for key := range records {
+		fmt.Fprintf(w,"%#v\n",records[key])
 	}
 }
