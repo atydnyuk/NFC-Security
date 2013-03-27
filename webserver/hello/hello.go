31a95f8b711652fa301ccfe9f4a50501fddd329a
atydnyuk <atydnyuk@gmail.com>
2013-03-27 00:35:16 -0400

    Made the log work on my own. Feels hacky. Looks ok, needs some work

--- webserver/hello/hello.go
+++ webserver/hello/hello.go
@@ -7,6 +7,7 @@ import (
 	"appengine"
 	"appengine/datastore"
 	"time"
+	"strings"
 )
 
 type RequestRecord struct {
@@ -14,6 +15,8 @@ type RequestRecord struct {
 	Host string
 	Method string
 	Header string
+	Path string
+	RawQuery string
 	URL string
 	Time time.Time
 }
@@ -32,14 +35,19 @@ func root(w http.ResponseWriter, r *http.Request) {
 }
 
 func logprinter(w http.ResponseWriter, r *http.Request) {
+	
 	stringURL := fmt.Sprintf("%#v",r.URL)
 	stringHeader := fmt.Sprintf("%#v",r.Header)
+	stringpath := fmt.Sprintf("%#v",r.URL.Path)
+	rawquery := fmt.Sprintf("%#v",r.URL.RawQuery)
 	c := appengine.NewContext(r)
     req := RequestRecord{
     RemoteAddr:r.RemoteAddr,
 	Host:r.Host,
 	Method:r.Method,
 	Header:stringHeader,
+	Path:stringpath,
+	RawQuery:rawquery,
 	URL:stringURL,
 	Time:time.Now(),
 	}
@@ -55,7 +63,27 @@ func logprinter(w http.ResponseWriter, r *http.Request) {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
+	fmt.Fprintf(w,"<html><head>")
+	fmt.Fprintf(w,"<link rel=\"stylesheet\"")
+	fmt.Fprintf(w,"href='templates/log.css'/>")
+	fmt.Fprintf(w,"</head>")
 	for key := range records {
-		fmt.Fprintf(w,"%#v\n",records[key])
+		fmt.Fprintf(w,"<div class=\"match\">")
+		fmt.Fprintf(w,"<div><span class=\"label\">")
+		fmt.Fprintf(w,"%s",records[key].RemoteAddr)
+		fmt.Fprintf(w,"</span></div>")
+		fmt.Fprintf(w,"<div class=\"contents\">")
+		cPath:=clipQuotes(records[key].Path)
+		cQuery:=clipQuotes(records[key].RawQuery)
+		if (len(cQuery)!=0) {
+			fmt.Fprintf(w,"%s\n",records[key].Host+cPath+"?"+cQuery)
+		} else {
+			fmt.Fprintf(w,"%s\n",records[key].Host+cPath+cQuery)
+		}
+		fmt.Fprintf(w,"</div></div>")
 	}
+}
+
+func clipQuotes(x string) string {
+	return strings.Join(strings.Split(x,"")[1:len(x)-1],"")
 }
