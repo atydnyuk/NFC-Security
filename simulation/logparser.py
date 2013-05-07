15f71359b83d4f68a1c116dd9d93dae0742cfd59
atydnyuk <atydnyuk@gmail.com>
2013-05-06 22:02:40 -0400

    generated more sim results, and added to writeup

--- simulation/logparser.py
+++ simulation/logparser.py
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/usr/bin/python
+
+from optparse import OptionParser
 
 def main():
     parser = OptionParser()
@@ -6,13 +8,33 @@ def main():
                       help="Choose input file", metavar="FILE")
     
     (options, args) = parser.parse_args()
-
+    
     f = open(options.filename,"r")
-    text = f.read().replace("\n", "")
-    
-    print text
-    
-    
+    text = f.read()
+    request_count = 0
+    good_count = 0
+    bad_count = 0
+    state = True
+    dropped_count = 0.0
+    for line in text.split("\n"):
+        if "request" in line:
+            request_count += 1
+            if "Good" in line:
+                if (not state):
+                    state=True
+                    dropped_count+=1
+                good_count+=1
+            if "Malicious" in line:
+                if (state):
+                    state=False
+                bad_count+=1
 
-if __name_=="__main__":
+    print "We recieved "+str(request_count)+" requests\n"
+    print "Of these, "+str(good_count)+" were good and "+str(bad_count)+" were malicious\n"
+    print "There were "+str(dropped_count)+" good requests dropped\n"
+    print "This is "+str((dropped_count/good_count)*100)+"% of the total\n"
+    
+    
+    
+if __name__=="__main__":
     main()
--- simulation/simulation.go
+++ simulation/simulation.go
@@ -7,28 +7,33 @@ import (
 )
 
 const (
-	NUMBER_OF_DAYS = 1 //number of days to run simulation
+	NUMBER_OF_DAYS = 30 //number of days to run simulation
 	REQUESTS_PER_DAY = 100 //the popularity of the application
-	MALICIOUS_USERS_FRACTION = .3 //fraction of the requests that are malicious
-
-	ENABLE_DEDICATED_ATTACKERS = true //enables dedicated web attackers
-	STRENGTH_OF_ATTACKERS = 5 //number of requests to bombard with when attack commences
-	ATTACKS_PER_DAY = 5 //number of times a day to perform dedicated attacks
-
+	MALICIOUS_USERS_FRACTION = .1 //fraction of users that deface the tag
+	NUMBER_OF_ATTACKERS = 10 //the malicious requests are split even
+	
 	//this specifies the granularity of the simulation. 
 	//the higher the number, the more precise the results
 	//and the longer the application takes to run
 	SECONDS_PER_DAY = 24 
 )
+
+var rep_map map[int]int
+
 func main() {
+	rep_map = make(map[int]int)
+		
+	
+	for i:= 0; i < NUMBER_OF_ATTACKERS; i++ {
+		rep_map[i] = 0
+	}
+
 	start := time.Now()
 	fmt.Printf("Starting simulation at %d\n",start.Unix())
 	//launch all of the routines
 	go good_guys(start)
 	go bad_guys(start)
-	if (ENABLE_DEDICATED_ATTACKERS) {
-		go attackers()
-	}
+
 	//wait until the correct time has elapsed
 	seconds_needed := float64(NUMBER_OF_DAYS * SECONDS_PER_DAY)
 	for (time.Since(start).Seconds() < seconds_needed) {
@@ -65,11 +70,13 @@ func bad_guys(t time.Time) {
 		time.Sleep(100 * time.Millisecond)
 		odds := rand.Float64()
 		if (odds < bad_per_second/10) {
-			fmt.Printf("Malicious request issued\n")
+			//we want to issue a malicious request from a bad guy
+			badguy_id := rand.Intn(NUMBER_OF_ATTACKERS)
+			rep_map[badguy_id] += 1
+			if (rep_map[badguy_id] < 5) {
+				fmt.Printf("Malicious request issued\n")
+			}
 		}
 		
 	}
-}
-
-func attackers() {
 }
